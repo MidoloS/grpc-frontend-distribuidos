240e9c20daf90d557bd8e74e83e2f0a7df7fd4d9
MidoloS <midolo.1912@gmail.com>
2023-09-07 16:17:41 -0300

    fix url

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -12,7 +12,7 @@ const handleSubmit = async (e) => {
 
   console.log("body", JSON.stringify(data));
 
-  const response = await fetch("https://localhost:7055/api/User/login", {
+  const response = await fetch("https://localhost:7055/api/user/login", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
